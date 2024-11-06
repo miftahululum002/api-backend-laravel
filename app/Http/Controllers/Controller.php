aff728e52f32ae9ad6c6f7f05c93e61f269a1888
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-06 08:59:11 +0700

    base function for reponse api agar seragam

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,19 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
 
-    protected function responseApi($response, $statusCode)
+    protected function responseSuccess($response, $statusCode = 200)
+    {
+        $response['success'] = true;
+        return $this->responseApi($response, $statusCode);
+    }
+
+    protected function responseError($response, $statusCode = 400)
+    {
+        $response['success'] = false;
+        return $this->responseApi($response, $statusCode);
+    }
+
+    private function responseApi($response, $statusCode)
     {
         return response()->json($response, $statusCode);
     }
