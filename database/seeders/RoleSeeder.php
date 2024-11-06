f11984ed3f17d14763cf4272910f2b2f0b5f3d5d
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-06 15:39:18 +0700

    role seeder untuk data dummy

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $lists = [
+            ['name' => 'Admin',],
+            ['name' => 'User',],
+        ];
+        Role::insert($lists);
+    }
+}
