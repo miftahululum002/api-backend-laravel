dae31a88814dd44b65dfb4cf91c90d4a064fb47f
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-06 09:27:16 +0700

    user crud handler

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $data = User::all();
+        return $this->responseSuccess('Data user', $data, 200);
+    }
+
+    public function show(User $user)
+    {
+        return $this->responseSuccess('Detail Data User', $user, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users,email',
+            'umur'      => 'nullable|numeric|min:1',
+            'password'  => 'required|min:8|confirmed'
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return $this->responseError('Tambah user gagal', $validator->errors(), 422);
+        }
+        $input = $validator->validated();
+        $userData = $input;
+        $userData['password'] = bcrypt($input['password']);
+        try {
+            $user = User::create($userData);
+            return $this->responseSuccess('Data User Berhasil Disimpan!', $user, 201);
+        } catch (Exception $e) {
+            return $this->responseError('Data User Gagal Disimpan!', $e->getMessage(), 500);
+        }
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'      => 'required',
+            "umur"      => 'nullable|numeric|min:1',
+            'email'     => 'required|email|unique:users,email,' . $user->id,
+            'password'  => 'nullable|min:8'
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return $this->responseError('Update user gagal', $validator->errors(), 422);
+        }
+        $input = $validator->validated();
+        $userUpdate = $input;
+        if ($request->password) {
+            $userUpdate['password'] = bcrypt($input['password']);
+        }
+        try {
+            $user->update($userUpdate);
+            return $this->responseSuccess('Data User Berhasil Diupdate!', $user, 200);
+        } catch (Exception $e) {
+            return $this->responseError('Data User Gagal Diupdate!', $e->getMessage(), 500);
+        }
+    }
+
+    public function destroy(User $user)
+    {
+        try {
+            $user->delete();
+            return $this->responseSuccess('Data User Berhasil Dihapus!', null, 200);
+        } catch (Exception $e) {
+            return $this->responseError('Data User Gagal Dihapus!', $e->getMessage(), 500);
+        }
+    }
+}
