f52f0f30c1cc870bdd73444dd4d0c02aae1e9a3c
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-06 10:00:34 +0700

    route api

---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Middleware\JwtMiddleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+Route::middleware(JwtMiddleware::class)->get('/user', function (Request $request) {
+    return response()->json([
+        'success' => true,
+        'message' => 'Data User',
+        'data' => $request->user(),
+        'error' => null,
+    ]);
+});
+
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::post('/logout', LogoutController::class)->name('logout');
+
+Route::middleware(JwtMiddleware::class)->group(function () {
+    Route::prefix('users')->group(function () {
+        Route::name('users.')->group(function () {
+            Route::controller(UserController::class)->group(function () {
+                Route::get('/', 'index')->name('index');
+                Route::get('/{user}', 'show')->name('show');
+                Route::post('/', 'store')->name('store');
+                Route::put('/{user}', 'update')->name('update');
+                Route::delete('/{user}', 'destroy')->name('destroy');
+            });
+        });
+    });
+});
