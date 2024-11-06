91cba82991f5468d1d2db257e3ce31447f0395e9
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-06 09:59:12 +0700

    set 404 page not found

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('{path}', function () {
+    abort(404, 'Page not found');
+})->where('path', '.*');
