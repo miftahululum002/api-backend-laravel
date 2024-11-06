c84ae7a39ef3a094a818009ab3ac470e9de8985e
O027-MIFTAHUL ULUM S.Pd <ulumiftahul06@gmail.com>
2024-11-06 08:57:13 +0700

    function reponse api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,9 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+
+    protected function responseApi($response, $statusCode)
+    {
+        return response()->json($response, $statusCode);
+    }
 }
